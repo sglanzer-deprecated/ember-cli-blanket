7de5da350dbabd48916038c1d7d60435a0d04d0e
Steven Glanzer <sglanzer@ciena.com>
2014-11-21 15:47:25 -0500

    Adding blanket configuration and qunit start
    Modifies index.html on ember generate ember-cli-blanket

--- blueprints/ember-cli-blanket/index.js
+++ blueprints/ember-cli-blanket/index.js
@@ -1,11 +1,41 @@
+/* globals module */
+
+var EOL = require('os').EOL;
+
 module.exports = {
-    normalizeEntityName: function() {
-        // this prevents an error when the entityName is
-        // not specified (since that doesn't actually matter
-        // to us
-    },
+    normalizeEntityName: function() {},
 
     afterInstall: function() {
-        return this.addBowerPackageToProject('blanket', '~1.1.5');
+        // Import blanket using bower
+        return this.addBowerPackageToProject('blanket', '~1.1.5')
+
+            // Modify tests/index.html to include blanket options
+            // and the call to start QUnit (since blanket turns autostart
+            // off to allow the instrumented modules to load)
+            .then(function() {
+                return this.insertIntoFile(
+                    'tests/index.html',
+
+                    '<script>' + EOL +
+                    '   blanket.options({' + EOL +
+                    '       antifilter: "-test.js"' + EOL +
+                    '   });' + EOL +
+                    '</script>' + EOL,
+
+                    { after: '<script src="assets/test-support.js"></script>' + EOL }
+                );
+            }.bind(this))
+
+            .then(function() {
+                return this.insertIntoFile(
+                    'tests/index.html',
+
+                        '<script>' + EOL +
+                        '   QUnit.start();' + EOL +
+                        '</script>' + EOL,
+
+                    { after: '<script src="assets/test-loader.js"></script>' + EOL }
+                );
+            }.bind(this));
     }
 };
