3ce0501f416c24193a5cb876e90e64a8359d31fe
Steven Glanzer <sglanzer@ciena.com>
2014-11-25 13:08:18 -0500

    Hacked module based coverage

--- blueprints/ember-cli-blanket/index.js
+++ blueprints/ember-cli-blanket/index.js
@@ -1,9 +1,20 @@
 /* globals module */
 
+var EOL = require('os').EOL;
+
 module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    return this.addBowerPackageToProject('blanket', '~1.1.5');
+    return this.addBowerPackageToProject('blanket', '~1.1.5')
+
+        // Modify tests/index.html to include the blanket loader after the application
+        .then(function() {
+            return this.insertIntoFile(
+                'tests/index.html',
+                '    <script src="assets/blanket-loader.js"></script>',
+                { after: '<script src="assets/' + this.project.config().modulePrefix + '.js"></script>' + EOL }
+            );
+        }.bind(this));
   }
 };
